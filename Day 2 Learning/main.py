eca96889a3a300055582a6e2c52e00ab044657d7
Mike Zapata <MikeZapata505@gmail.com>
2026-01-17 18:50:32 -0500

    Logical Operator exercise

---
+++ Day 2 Learning/main.py
@@ -0,0 +1,12 @@
+# Logical Operators
+    # and = both conditions must be true
+    # or = at least one condition must be true
+    # not = inverts the boolean value
+
+temperature = 25
+is_sunny = False
+
+if temperature >= 20 and is_sunny:
+    print("It's a good day for a walk!")
+else:
+    print("It's not a good day for a walk.")
